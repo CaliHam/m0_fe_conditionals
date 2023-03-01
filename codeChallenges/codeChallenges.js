663a78d981d130ae891357bd6e1970a93105f877
Calli Herrmann <calliherrmann@gmail.com>
2023-03-01 16:22:43 -0500

    Add medium challenge excerise

--- codeChallenges/codeChallenges.js
+++ codeChallenges/codeChallenges.js
@@ -14,3 +14,25 @@ if (episodesWatched % 2 === 0) {
 // The remainder operator (%) does long division to see if there is a remainder leftover 
 // after  it completely divides. If there is a remainder, it is odd. If there is no remainder (0)
 // then it is even.
+
+// MEDIUM
+// Using the following variables:
+
+var goodDrivingRecord = true;
+var age = 23;
+
+
+// Write a program that evaluates the status of the variables above, and uses logic to print one of three possible messages:
+// - If the driving record is good and user is over 25 years old, they should get a discount on the car rental
+// - If the user either has a good record or is over 25 years old, they should pay full price
+// - If the user is not over 25 and has a bad driving record, they need to have someone else sign for the rental
+
+if (goodDrivingRecord === true && age >= 25) {
+    console.log("Congratulations! You get a discount.");
+} else if (goodDrivingRecord === true || age >= 25) {
+    console.log("Pay full price.");
+} else {
+    console.log("Have someone else sign for the rental.");
+};
+
+// if (goodDrivingRecord === false || age < 25) 
